67e91c3d2cdd20bbccf8a81bfec687c0467f29a0
Jitendra <jitendra01432@gmail.com>
2024-11-11 01:00:04 +0530

    node file read

--- cybrom.jsx
+++
@@ -1,8 +0,0 @@
-exports.myCollege=()=>{
-    return "<h3> we are mern devloper in student </h3>"
-}
-
-
-exports.myFees=()=>{
-    return " <h2> my fees is 56000/- per day </h2>"
-}
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const http = require("http")
-const myclg =require("./cybrom.jsx")
+
+const http = require("http");
+const fs = require("fs")
 
 http.createServer((req,res)=>{
-    res.write("<h2> welcome </h2>")
-    res.write(myclg.myCollege())
-    res.write(myclg.myFees())
-    res.end("<h2>.....server end ... </h2>");
-}).listen(9000)
+    fs.readFile("ayushi.txt",(err,data)=>{
+
+        res.write(data);
+        res.end();
+    })
+
+}).listen(8000)
